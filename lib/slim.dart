91c5f4e46f37af5de5f4f574ea35a63c008c0211
Gadi Perry <gadon1@gmail.com>
2020-05-31 00:58:53 +0300

    3.0.6

--- lib/slim.dart
+++ lib/slim.dart
@@ -1,7 +1,7 @@
 library slim;
 
-export 'state_management.dart';
-export 'extensions.dart';
-export 'localization.dart';
-export 'rest_api.dart';
-export 'builders.dart';
+export 'src/state_management.dart';
+export 'src/extensions.dart';
+export 'src/localization.dart';
+export 'src/rest_api.dart';
+export 'src/builders.dart';
