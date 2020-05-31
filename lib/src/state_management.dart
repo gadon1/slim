2dce9820d7c3bb7dd5dc21f7753c474491618724
Gadi Perry <gadon1@gmail.com>
2020-05-31 10:06:36 +0300

    3.1.4

--- lib/src/state_management.dart
+++ lib/src/state_management.dart
@@ -29,7 +29,7 @@ class Slimer<T> {
   Widget slim(Widget child) => Slim<T>(child: child, stateObject: stateObject);
 }
 
-/// Useful extension methods for [List<Slimer>]
+/// Useful extension methods on List<[Slimer]>
 extension SlimSlimersX on List<Slimer> {
   Widget slim({@required Widget child}) =>
       fold(null, (value, slimer) => slimer.slim(value ?? child));
